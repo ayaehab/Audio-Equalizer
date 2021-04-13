74c26daf9bcf5727d989a6c266fd62b1f56017b3
Aya Ehab <61461463+ayaehab@users.noreply.github.com>
2021-04-13 21:42:40 +0200

    Fourrier and equalizer are added

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from matplotlib.figure import Figure
 
 import sys
 import numpy as np
+from numpy.fft import fft, ifft
 import pandas as pd
 from pyqtgraph import PlotWidget, PlotItem
 import pyqtgraph as pg
@@ -36,7 +37,13 @@ class AudioEqualizer(QtWidgets.QMainWindow):
 
         self.actionOpen_2.triggered.connect(lambda: self.open_file())
         self.pens = [pg.mkPen('r'), pg.mkPen('b'), pg.mkPen('g')]
-
+        
+        self.sliderList = [ self.Slider_1, self.Slider_2, self.Slider_3, self.Slider_4, self.Slider_5,
+                              self.Slider_6, self.Slider_7, self.Slider_8, self.Slider_9, self.Slider_10 ]
+        self.bands=[]
+        self.gain=[]
+        
+        
     # Open (.wav ) file, read it using Scipy Lib, and plot it in inputSignal Viewer
     def open_file(self):
         self.selected_file = QtGui.QFileDialog.getOpenFileName(
@@ -48,17 +55,19 @@ class AudioEqualizer(QtWidgets.QMainWindow):
             # Read selected wav file
             self.samplerate, self.data = wavfile.read(
                 str(self.selected_file[0]))
-
+            
+            self.length = self.data.shape[0] #number of samples
             # The duration is equal to the number of frames divided by the framerate (frames per second)
-            self.duration = self.data.shape[0] / self.samplerate
+            self.duration = self.length / self.samplerate
 
             # Return evenly spaced numbers over a specified interval
-            self.time = np.linspace(0., self.duration, self.data.shape[0])
+            self.time = np.linspace(0., self.duration, self.length)
+            self.freq = np.linspace(0, self.samplerate / 2, int(self.length / 2))
 
             # Plot first channel's signal
-            self.InputSignal.plot(self.time, self.data[:, 0], pen=self.pens[0])
-            # Plot second channel's signal
-            self.InputSignal.plot(self.time, self.data[:, 1], pen=self.pens[1])
+            # self.InputSignal.plot(self.time, self.data[:, 0], pen=self.pens[0])
+            # # Plot second channel's signal
+            # self.InputSignal.plot(self.time, self.data[:, 1], pen=self.pens[1])
 
         else:
             QMessageBox.warning(self.centralWidget,
@@ -68,7 +77,54 @@ class AudioEqualizer(QtWidgets.QMainWindow):
         for i in range(1, len(s)):
             if s[-i] == '.':
                 return s[-(i - 1):]
-
+            
+#*******************************************Fourrier**************************************#  
+       
+    def get_fft (self):
+        # fft returns an array contains all +ve values then all -ve values
+        # it has some real and some complex values
+        self.fftArray = fft(self.data) 
+        #separate the +ve from the -ve
+        self.fftArrayPositive = self.fftArray[:self.length // 2]
+        self.fftArrayNegative = np.flip(self.fftArray[self.length // 2:])
+        #get the magnitude of both +ve & -ve
+        self.fftArrayAbs = np.abs(self.fftArray)
+        self.fftMagnitude = self.fftArrayAbs[: self.length // 2] # magnitude of +ve only
+        
+        # plt.plot(self.freq , self.fftMagnitude)
+        # plt.show()
+        
+        # return fftArray that will be used later in the inverse forrier transform
+        # and fftMagnitude that will be used in plotting ....
+        return self.fftArray , self.fftMagnitude
+    
+    
+    def get_ifft(self,fftArray):
+        
+        self.ifftArray = ifft(self.fftArray).real 
+        # plt.plot(self.time , self.ifftArray)
+        # plt.show()      
+        return self.ifftArray
+    
+#*******************************************END OF Fourrier**************************************# 
+   
+    def slider(self):
+        
+        self.fMagnitude = self.get_fft()[1]
+        self.valuePerBand=int(len(self.fMagnitude)/10)
+        #print(self.valuePerBand)
+        
+        for i in range(10):
+            self.bands.append(self.fMagnitude[i * self.valuePerBand : min(len(self.fMagnitude)+1, (i+1)*self.valuePerBand)])
+     
+    def equalizer(self):
+        self.newMagnitude = []
+        for i in range(10):
+            self.gain.append(self.sliderList[i].value())
+        
+        for index in range(10):
+            Magnified_Magnitued = self.gain[i] * np.array(self.bands[i]) #we changed it to np.array so we can multiply the value by value not multipling the list that will generate repetation of value not multplication
+            self.newMagnitude.append(list(Magnified_Magnitued))
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
