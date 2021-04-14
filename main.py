6f04341a0d3b240b3f9eb9b70ca655f6fc2e1846
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2021-04-14 23:52:14 +0200

    plot any dimension
    added if conditions to plot all wav files normally

--- main.py
+++ main.py
@@ -24,10 +24,9 @@ import matplotlib.ticker as ticker
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas,  NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from pdf import GeneratePDF
-import img_rc #for gui
+import img_rc  # for gui
 
 matplotlib.use('Qt5Agg')
-
 
 
 class AudioEqualizer(QtWidgets.QMainWindow):
@@ -42,7 +41,7 @@ class AudioEqualizer(QtWidgets.QMainWindow):
         self.Play_Button.clicked.connect(lambda: self.play())
         self.Stop_Button.clicked.connect(lambda: self.stop())
         self.pens = [pg.mkPen('r'), pg.mkPen('b'), pg.mkPen('g')]
-        
+
         self.sliderList = [self.Slider_1, self.Slider_2, self.Slider_3, self.Slider_4, self.Slider_5,
                            self.Slider_6, self.Slider_7, self.Slider_8, self.Slider_9, self.Slider_10]
 
@@ -67,7 +66,6 @@ class AudioEqualizer(QtWidgets.QMainWindow):
     def make_new_window(self):
         self.new_win = AudioEqualizer()
         self.new_win.show()
-
 
     def plot_spectrogram(self, data_col, viewer):
         # im not sure how to compute fs, default value for this task will be 10e3
@@ -135,7 +133,7 @@ class AudioEqualizer(QtWidgets.QMainWindow):
         if self.file_ext == 'wav':
             # Read selected wav file
             self.samplerate, self.data = wavfile.read(path)
-
+            np.array(self.data)
             self.length = self.data.shape[0]  # number of samples
             # The duration is equal to the number of frames divided by the framerate (frames per second)
             self.duration = (self.length / self.samplerate)
@@ -145,14 +143,24 @@ class AudioEqualizer(QtWidgets.QMainWindow):
 
             self.freq = fftfreq(self.length, 1 / self.samplerate)
 
-            # Plot first channel's signal
-            self.InputSignal.plot(self.time, self.data[:, 0], pen=self.pens[0])
-            # # Plot second channel's signal on the first one with different color
-            self.InputSignal.plot(self.time, self.data[:, 1], pen=self.pens[1])
+            if np.ndim(self.data) == 2:
+                # Plot first channel's signal
+                self.InputSignal.plot(
+                    self.time, self.data[:, 0], pen=self.pens[0])
+                # # Plot second channel's signal on the first one with different color
+                self.InputSignal.plot(
+                    self.time, self.data[:, 1], pen=self.pens[1])
+                # Plot the spectrogram for the input in InputSpectro Viewer
+                self.plot_spectrogram(self.data[:, 0], self.InputSpectro)
+            elif np.ndim(self.data) == 1:
+                self.InputSignal.plot(self.time, self.data, pen=pg.mkPen('r'))
+                self.plot_spectrogram(self.data, self.InputSpectro)
+            else:
+                QMessageBox.warning(self.centralWidget,
+                                    "Your .wav file cannot be plotted")
+
             self.InputSignal.setLimits(
                 xMin=0, xMax=500000, yMin=-200000, yMax=200000)
-            # Plot the spectrogram for the input in InputSpectro Viewer
-            self.plot_spectrogram(self.data[:, 0], self.InputSpectro)
 
         else:
             QMessageBox.warning(self.centralWidget,
@@ -164,7 +172,7 @@ class AudioEqualizer(QtWidgets.QMainWindow):
 
     def create_my_pdf(self):
         if self.InputSignal.scene():
-            #export all items in all viewers as images 
+            # export all items in all viewers as images
             exporter1 = pg.exporters.ImageExporter(self.InputSignal.scene())
             exporter1.export('input_signal.png')
 
@@ -251,7 +259,6 @@ class AudioEqualizer(QtWidgets.QMainWindow):
         self.inverse = np.fft.irfft(finalSignal)
         self.OutputSignal.setYRange(min(self.inverse), max(self.inverse))
         self.OutputSignal.plot(self.inverse, pen=pg.mkPen('y'))
-
 
     def play(self):
         sd.play(self.data, self.samplerate)
